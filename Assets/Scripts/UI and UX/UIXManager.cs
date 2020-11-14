a449209d3af6ddfaf4e0fdef809de295f69f0fb0
darlynn-best <70346167+darlynn-best@users.noreply.github.com>
2020-11-14 16:00:44 -0600

    Menu mostly done

---
+++ Assets/Scripts/UI and UX/UIXManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class UIXManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Loads a scene with the given name
+    /// </summary>
+    /// <param name="sceneName">Name of the scene to load</param>
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    /// <summary>
+    /// Quits the game
+    /// </summary>
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
