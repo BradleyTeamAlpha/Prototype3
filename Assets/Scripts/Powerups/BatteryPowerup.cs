e62a8789c573681cd06d13c9ec73624f3e5123d8
Doug Guzman <swiftshadow34@gmail.com>
2020-11-15 19:13:56 -0600

    Adds battery powerup

---
+++ Assets/Scripts/Powerups/BatteryPowerup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryPowerup : PowerupBase
+{
+    private PlayerManager playerManager;
+
+    public float healthGain;
+
+    protected override void DoPowerup()
+    {
+        playerManager.Health += healthGain;
+    }
+}
