873793dd7726984cdb726153fa18af5ce741bb16
Doug Guzman <swiftshadow34@gmail.com>
2020-11-15 18:31:03 -0600

    Implements more fancy art stuffs

--- Assets/Scripts/Backgrounds/BackgroundScroll.cs
+++ Assets/Scripts/Backgrounds/BackgroundScroll.cs
@@ -19,9 +19,9 @@ public class BackgroundScroll : MonoBehaviour
         Vector2 newPos = transform.position;
         newPos.x -= (gameManager.speed/scrollDivisor) * Time.deltaTime;//should parallax the backgrounds
         transform.position = newPos;
-        if(transform.position.x < -23.92)//replace -18 with magic number of background size with Charli. Trial and error baby!
+        if(transform.position.x < -19)//replace -18 with magic number of background size with Charli. Trial and error baby!
         {
-            Vector3 bgFix = new Vector3(23.92f, 0, 0);
+            Vector3 bgFix = new Vector3(19f, 0, 0);
             transform.position = bgFix;//could change
             Sprite temp = gameManager.NextBackground();
             gameObject.GetComponent<SpriteRenderer>().sprite = temp;//should set sprite to whatever game object gave-can do fancy logic in function later
