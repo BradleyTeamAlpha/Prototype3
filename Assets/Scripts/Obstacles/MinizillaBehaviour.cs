2e5e5bd2d3fd32fdb4327c13a12564e97642bf71
Doug Guzman <swiftshadow34@gmail.com>
2020-12-07 20:56:45 -0600

    Various minor bug fixes

--- Assets/Scripts/Obstacles/MinizillaBehaviour.cs
+++ Assets/Scripts/Obstacles/MinizillaBehaviour.cs
@@ -18,7 +18,7 @@ public class MinizillaBehaviour : ObstacleBehaviour
         {
             yield return new WaitForSeconds(data.timeBetweenShots);
             Vector2 shootPos = transform.position;
-            shootPos.y += 0.9f;
+            shootPos.y += 0.8f;
             Instantiate(data.projectile, shootPos, Quaternion.Euler(0, 0, 270));
         }
     }
